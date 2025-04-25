c331c5929280267e51cc846872b0648132702c5f
Flaman <irakozeflamanc@gmail.com>
2025-04-25 12:15:37 +0200

    feat:implement register user and also make login

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
     "net/http"
     "todo-list/config"
     "todo-list/routes"
+    "github.com/joho/godotenv"
 )
 
 func main() {
+
+       // Load .env file
+       err := godotenv.Load()
+       if err != nil {
+           log.Fatal("Error loading .env file")
+       }
+   
     config.ConnectDB()
 
     // Register all routes
